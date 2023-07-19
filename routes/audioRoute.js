bedc85176e4de7e9d8f85703031cdd0309417c2e
Prashanth <yesyesprashanth@gmail.com>
2023-07-19 12:04:20 +0530

    audioroute file update

--- routes/audioRoute.js
+++ routes/audioRoute.js
@@ -6,8 +6,7 @@ import fs from 'fs';
 route.get("/", (req,res)=>{
 
     const filenameWithfoldername = req.body.filename;
-    res.sendFile(path.resolve('./audiofiles/' + filenameWithfoldername));   
-    // res.json(filenameWithfoldername);
+    res.sendFile(path.resolve('./audiofiles/' + filenameWithfoldername));      
 });
 
 
